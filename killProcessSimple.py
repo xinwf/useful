d0d49d5b570cb15c42cefca4519f25f48b2ae8bd
xinwenfei <xinwenfei@ismart-bot.com>
2018-10-24 15:01:43 +0800

    add another version of python script for killing process

---
+++ killProcessSimple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#-*-encoding:utf-8-*-
+#usage: ./scriptprogram processname
+import os,sys
+processLine = os.popen("ps aux | grep %s" % sys.argv[1])
+line = processLine.readline()
+
+while line != '':
+    if 'grep' in line or sys.argv[0] in line:
+        line = processLine.readline()
+        continue
+    if sys.argv[1] in line :
+        print("kill process: %s" % " ".join(line.split()[10:]))
+        ret = os.system("kill -9 %s" % line.split()[1])
+    line = processLine.readline()
+

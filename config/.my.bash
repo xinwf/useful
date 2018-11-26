38a16128e2baa47d15c8baa4af1e90b76416d826
辛文飞 <41889613+xinwf@users.noreply.github.com>
2018-11-26 11:10:52 +0800

    Update .my.bash

--- config/.my.bash
+++ config/.my.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-APOLLO_HOME='/home/ubuntu/auto/apollo'
-APOLLO3_HOME='/home/ubuntu/apollo/apollo3.0/apollo'
+APOLLO_HOME='/home/ubuntu/git_intergrate'
+APOLLO3_HOME='/home/ubuntu/apollo3/ZRCar100/apollo3'
 
 #general
 alias cln='printf "\033c"'
@@ -16,12 +16,14 @@ alias stopdocker='docker stop $(docker ps -aq)'
 alias clnlog='rm -rf ${APOLLO_HOME}/data/log/*'
 alias clncore='rm -rf ${APOLLO_HOME}/data/core/*'
 alias startdocker='bash ${APOLLO_HOME}/docker/scripts/dev_start.sh -C -l -t apollo'
+alias startdockerper='bash ${APOLLO_HOME}/docker/scripts/dev_start_per.sh -C -l -t apollo-per'
 alias intodocker='bash ${APOLLO_HOME}/docker/scripts/dev_into.sh'
 
-#apollo3.0 and docker relative
+#apollo3.0 relative
 alias clnlog3='rm -rf ${APOLLO3_HOME}/data/log/*'
 alias clncore3='rm -rf ${APOLLO3_HOME}/data/core/*'
 alias startdocker3='bash ${APOLLO3_HOME}/docker/scripts/dev_start.sh -C -l -t apollo'
+alias startdockerper3='bash ${APOLLO3_HOME}/docker/scripts/dev_start_per.sh -C -l -t apollo3.0-per'
 alias intodocker3='bash ${APOLLO3_HOME}/docker/scripts/dev_into.sh'
 
 #git relative
@@ -37,5 +39,5 @@ alias gfc='git fetch'
 alias glg='git log'
 alias grm='git rm'
 
-alias kil='${APOLLO_HOME}/third_party/killProcess.py'
-alias skil='sudo ${APOLLO_HOME}/third_party/killProcess.py'
+alias kil='${APOLLO_HOME}/module/tools/ismart/killProcess.py'
+alias skil='sudo ${APOLLO_HOME}/module/tools/ismart/killProcess.py'

40079ddaa1fadd9945e8e4e46f89e7ef88ce6f83
xinwenfei <xinwenfei@ismart-bot.com>
2019-04-14 17:06:18 +0800

    update

--- python/killProcess.py
+++ python/killProcess.py
@@ -4,22 +4,13 @@
 import os,sys
 processLine = os.popen("ps aux | grep %s" % sys.argv[1])
 line = processLine.readline()
-arr = line.split()
-col = 0
-for i in range(len(arr)):
-    if sys.argv[1] in arr[i]:
-        col = i
-        break
-#if the script doesn't find the process, then quit
-if not col:
-    exit()
+
 while line != '':
-    split1 = line.split()
-    if sys.argv[1] in split1[col] :
-        ret = os.system("kill -9 %s" % split1[1])
-        # print (ret)
-        if not ret:
-            print ("killed %s,Process name:%s" % (split1[1], split1[col][:-1]) )
+    if 'grep' in line or sys.argv[0] in line:
+        line = processLine.readline()
+        continue
+    if sys.argv[1] in line :
+        print("kill process: %s" % " ".join(line.split()[10:]))
+        ret = os.system("kill -9 %s" % line.split()[1])
     line = processLine.readline()
-    if 'grep' in line:
-        break
+    

8769fb99b48f3f95dcb75579a473f2dee7f75b28
xinwenfei <xinwenfei@126.com>
2019-05-09 15:37:13 +0800

    update my.bash

--- config/.my.bash
+++ config/.my.bash
@@ -13,6 +13,10 @@ alias monnvi='watch -d -n 1 nvidia-smi'
 alias conz420='ssh ubuntu@192.168.0.99'
 alias psgc='ps aux | grep '
 alias getOneDir='python ${HOME}/Public/xwf_github/useful/getOneDirV2/getOneDir.py'
+alias lsf='ls -l |grep "^-"'
+alias lsd='ls -l |grep "^d"'
+alias wcf='ls -l |grep "^-" | wc -l'
+alias wcd='ls -l |grep "^d" | wc -l'
 
 #docker relative
 alias clndocker='docker volume rm $(docker volume ls -qf "dangling=true")'

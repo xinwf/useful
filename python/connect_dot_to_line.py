7358e30abda911564d5b7e95ffb0144f31c7ca39
xinwenfei <xinwenfei@126.com>
2019-06-11 16:03:30 +0800

    Add flexibility

--- python/connect_dot_to_line.py
+++ python/connect_dot_to_line.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python
 
+import sys
 import matplotlib.pyplot as plt
 
-f = open("current_goal_ipa_apartment.txt", "r")
+f = open(sys.argv[1], "r")
 data = f.readlines()
 f.close()
 x_list = []
 y_list = []
-read = False
-for line in data:
-    if "position" in line:
-        read = True
-        continue
-    elif "orientation" in line:
-        read = False
-        continue
-    elif read:
-        if "x:" in line:
-            x_list.append(float(line.split(":")[1].replace("\n","").replace(" ", "")))
-        elif "y:" in line:
-            y_list.append(float(line.split(":")[1].replace("\n","").replace(" ", "")))
-        # elif len(y_list) <10:
-        #     continue
-        # else:
-        #     break
+
+def readComplex(data):
+    read = False
+    for line in data:
+        if "position" in line:
+            read = True
+            continue
+        elif "orientation" in line:
+            read = False
+            continue
+        elif read:
+            if "x:" in line:
+                x_list.append(float(line.split(":")[1].replace("\n","").replace(" ", "")))
+            elif "y:" in line:
+                y_list.append(float(line.split(":")[1].replace("\n","").replace(" ", "")))
+            # elif len(y_list) <10:
+            #     continue
+            # else:
+            #     break
+
+def readSimple(data):
+    for line in data:
+        rd = line.replace("\n", "").split(" ")
+        x_list.append(rd[0])
+        y_list.append(rd[1])
 
 # print(x_list)
 # print(y_list)
+readSimple(data)
 plt.plot(x_list, y_list, color='g')
 plt.show()

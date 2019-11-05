370cd142cb3f78b8e1e5d27ab7b6ce38bab5d433
辛文飞 <41889613+xinwf@users.noreply.github.com>
2019-11-05 15:45:51 +0800

    Update deploy_ros_env.sh

--- install/deploy_ros_env.sh
+++ install/deploy_ros_env.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+using_ali_source()
+{
+    sudo mv /etc/apt/sources.list /etc/apt/sources.list.old
+    sudo sh -c 'echo \
+"# deb cdrom:[Ubuntu 16.04 LTS _Xenial Xerus_ - Release amd64 (20160420.1)]/ xenial main restricted
+deb-src http://archive.ubuntu.com/ubuntu xenial main restricted #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse #Added by software-properties
+deb http://archive.canonical.com/ubuntu xenial partner
+deb-src http://archive.canonical.com/ubuntu xenial partner
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted multiverse universe #Added by software-properties
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security universe
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security multiverse" > /etc/apt/sources.list'
+    sudo apt update
+}
+
 add_ros_source_and_base()
 {
     echo "Add ros source ..."
@@ -10,6 +35,7 @@ add_ros_source_and_base()
     else
         echo "Ros source has been added."
     fi
+    sudo apt update
     sudo apt-get install ros-kinetic-ros-base -y
 }
 
@@ -97,6 +123,8 @@ enable_ssh_server()
     sudo apt-get install -y openssh-server
     sudo service sshd start
 }
+
+using_ali_source
 
 add_ros_source_and_base
 

ce4cd4af73e686c98ef61b0588079bfc3be611af
xinwenfei <xinwf18@gmail.com>
2021-01-05 14:22:12 +0800

    update download url and logic

--- shell/get-bing-picture.sh
+++ shell/get-bing-picture.sh
@@ -16,13 +16,15 @@
 #
 
 # 定义图片基本 URL
-SET_URL_BASE="https://bing.com"
+SET_URL_BASE="https://cn.bing.com"
 # 定义 API 地址
 SET_API="https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
 
 # Define two different versions
-SET_API_EN="https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&ensearch=1"
+SET_API_EN="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&ensearch=1"
 SET_API_CN="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&ensearch=0"
+
+WallpaperBackup=$HOME/.config/Wallpaper/Backups
 
 # The following setting is pretty significant, due to the cron uses /bin/sh as the script's interpreter, which doesn't
 # support sufficient environment variable, hence, we do some work to fix it(This was added for ubuntu mate)
@@ -32,10 +34,10 @@ SET_API_CN="https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&ensearch
 # 准备工作
 Preparation_Action() {
     # 判断存放壁纸的文件夹是否存在，如果不存在就创建
-    if [ -d "$HOME/.config/Wallpaper/Backups" ]; then
+    if [ -d "$WallpaperBackup" ]; then
         echo "hello"
     else
-        mkdir -p "$HOME/.config/Wallpaper/Backups"
+        mkdir -p "$WallpaperBackup"
     fi
 }
 
@@ -55,7 +57,7 @@ Set_Wallpaper() {
     LAST_PIC_FILE_NAME=$TODAY"_"$FILE_NAME$FILE_TYPE
 
     # 复制壁纸到壁纸的存放目录
-    cp -rf "$HOME/.config/Wallpaper/Backups/$LAST_PIC_FILE_NAME" "$HOME/.config/Wallpaper/Current$FILE_TYPE"
+    cp -f "$WallpaperBackup/$LAST_PIC_FILE_NAME" "$HOME/.config/Wallpaper/Current$FILE_TYPE"
     # 通过 feh 设置壁纸
     # 你也可以注释掉下面这行，转而使用其它壁纸设置工具，如果你想的话
     # feh --randomize --no-fehbg --bg-scale ~/.config/Wallpaper/Current$FILE_TYPE
@@ -67,23 +69,37 @@ Set_Wallpaper() {
     gsettings set org.cinnamon.desktop.background picture-uri "file://$(find $HOME/.config/Wallpaper -maxdepth 1 -type f | shuf -n1)"
 }
 
+CheckIfExist() {
+    existed=false
+    for fn in $(ls $WallpaperBackup)
+    do
+        pic_name=${fn#*_}
+        pic_name=${pic_name%.*}
+        if [ "$1" == "$pic_name" ];then
+            existed=true
+            break
+        fi
+    done
+    echo $existed
+}
+
 # 获取图片链接地址
 Get_Pictures() {
     # 获取图片具体 URL(default)
-    GET_URL=$(curl -sL "$SET_API_CN" | awk -F '"' '{ print $18 }')
+    # GET_URL=$(curl -sL "$SET_API_CN" | awk -F '"' '{ print $18 }')
     
     # set two timers with crontab
-    # hour=$(date +%H)
+    hour=$(date +%H)
     # minute=$(date +%M)
-    # if [ $hour -gt 13 ]; then
-    # if [ $minute -lt 12 ]; then
-        # echo "morning"
-        # GET_URL=$(curl -sL "$SET_API_EN" | awk -F '"' '{ print $18 }')
-    # else
-        # echo "afternoon"
-        # GET_URL=$(curl -sL "$SET_API_CN" | awk -F '"' '{ print $18 }')
-        # echo $GET_URL
-    # fi
+    # if [ $minute -gt 10 ]; then
+    if [ $hour -lt 13 ]; then
+        echo "morning"
+        GET_URL=$(curl -sL "$SET_API_EN" | awk -F '"' '{ print $18 }')
+    else
+        echo "afternoon"
+        GET_URL=$(curl -sL "$SET_API_CN" | awk -F '"' '{ print $18 }')
+    fi
+
     # 获取图片名
     GET_NAME=$(echo -e "$GET_URL" | cut -d "_" -f 1 | cut -d "." -f 2)
     # 获取图片格式
@@ -92,11 +108,15 @@ Get_Pictures() {
     # 获取当前日期
     TODAY=$(date +%Y%m%d)
 
-    # 下载图片
-    curl -L "$SET_URL_BASE$GET_URL" -o "$HOME/.config/Wallpaper/Backups/$TODAY"_"$GET_NAME.$GET_TYPE"
+    if [ "`CheckIfExist $GET_NAME`" != true ];then
+        # 下载图片
+        curl -L "$SET_URL_BASE$GET_URL" -o "$WallpaperBackup/$TODAY"_"$GET_NAME.$GET_TYPE"
 
-    # 设置壁纸
-    Set_Wallpaper "$GET_NAME" "$GET_TYPE" "$TODAY"
+        # 设置壁纸
+        Set_Wallpaper "$GET_NAME" "$GET_TYPE" "$TODAY"
+    else
+        echo $GET_NAME" exists"
+    fi
 }
 
 # 开始执行 get_Pictures 函数
